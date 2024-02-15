52606607f2132df4c9751fe19ba669d51cb5736a
Maza455 <same7nte@gmail.com>
2024-02-15 09:19:54 +0200

    Adding a config file

--- config/index.js
+++ config/index.js
@@ -1,1 +1,17 @@
 import { createPool } from "mysql";
+import { config } from "dotenv";
+
+let connection = createPool({
+    host: process.env.HOST,
+    database: process.env.DBName,
+    user: process.env.userName,
+    password: process.env.UserPass,
+    multipleStatements: true,
+    connectionLimit: 30
+})
+
+config()
+
+export {
+    connection
+}
